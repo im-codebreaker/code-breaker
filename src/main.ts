1548075ea98d46961e82677bd9031c5d7a2b449d
MehdiHan <meh.bouchard@outlook.fr>
2022-04-29 17:03:55 +0200

    chore: add markdown and styles for header

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
 import './style.scss'
 console.log('Hello World');
+
+const burgerButton: HTMLButtonElement | null = document.querySelector('.header_navbar-burger');
+
+burgerButton?.addEventListener('click', (): void => {
+  burgerButton.classList.toggle('open');
+})
