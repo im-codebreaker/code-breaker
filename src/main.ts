266e0fab25fddff5a6f375d7337ad86b7391df82
MehdiHan <meh.bouchard@outlook.fr>
2022-05-02 11:01:25 +0200

    chore: add custom mouse pointer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import './style.scss'
 console.log('Hello World');
 
+interface Cursor {
+  x: number,
+  y: number,
+  element: HTMLDivElement | null
+}
+const cursorDot: Cursor = {
+  x: 0,
+  y: 0,
+  element: document.querySelector('.cursor_dot')
+}
 const burgerButton: HTMLButtonElement | null = document.querySelector('.header_navbar-burger');
 
 burgerButton?.addEventListener('click', (): void => {
   burgerButton.classList.toggle('open');
 })
+
+window.addEventListener('load', () => {
+  if (cursorDot.element) {
+    const links: NodeListOf<HTMLAnchorElement> | null = document.querySelectorAll('a');
+
+    links.forEach((link: HTMLAnchorElement): void => {
+      link.addEventListener('mouseenter', () => {
+        cursorDot.element.classList.add('enter');
+      })
+      link.addEventListener('mouseleave', () => {
+        cursorDot.element.classList.remove('enter');
+      })
+    })
+
+    document.addEventListener('mousemove', (event: MouseEvent) => {
+      cursorDot.element.style.top = `${event.y}px`
+      cursorDot.element.style.left = `${event.x}px`
+    })
+  } else {
+    document.documentElement.style.cursor = 'default';
+  }
+})
+
